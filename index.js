1fe831abaee3665d175dc0c608584d970cd2dff9
Adrian Iuliu Moldovan <adrianiuliu.moldovan@yahoo.com>
2023-06-14 21:15:43 +0300

    add show Home and show Skills functions

---
+++ index.js
@@ -0,0 +1,9 @@
+var page = document.getElementById("home");
+page.style.display = "block";
+console.log("home", home);
+
+function showSkills() {
+  var page = document.getElementById("skills");
+  page.style.display = "block";
+}
+console.log("skills", skills);
