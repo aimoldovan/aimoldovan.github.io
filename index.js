66d22009cc0e72879443cc2aa4c9698c8846d475
Adrian Iuliu Moldovan <adrianiuliu.moldovan@yahoo.com>
2023-06-16 20:01:07 +0300

    remove custom all showx functions and used only showPage(id)

--- index.js
+++ index.js
@@ -5,28 +5,31 @@ function hideAllPages() {
   document.getElementById("projects").style.display = "none";
 }
 
-function showHome() {
+// // function showHome() {
+// //   hideAllPages();
+// //   var page = document.getElementById("home");
+// //   page.style.display = "block";
+// // }
+
+// // function showSkills() {
+// //   // document.getElementById("home").style.display = "none";
+// //   hideAllPages();
+// //   var page = document.getElementById("skills");
+// //   page.style.display = "block";
+// }
+
+// function showLanguages() {
+//   showPage("languages");
+// }
+// function showProjects() {
+//   showPage("projects");
+// }
+
+function showPage(id) {
   hideAllPages();
-  var page = document.getElementById("home");
+  var page = document.getElementById(id);
+  console.info(showPage);
   page.style.display = "block";
 }
 
-function showSkills() {
-  // document.getElementById("home").style.display = "none";
-  hideAllPages();
-  var page = document.getElementById("skills");
-  page.style.display = "block";
-}
-
-function showLanguages() {
-  hideAllPages();
-  var page = document.getElementById("languages");
-  page.style.display = "block";
-}
-function showProjects() {
-  hideAllPages();
-  var page = document.getElementById("projects");
-  page.style.display = "block";
-}
-
-showHome();
+showPage("home");
